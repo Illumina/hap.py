fb2c396f3d652e9e66f8c9ff226a464440179d1a
Peter Krusche <pkrusche@illumina.com>
2016-02-10 15:08:55 +0000

    HAP-155 automatically detect fixchr switches from reference fasta contigs

---
+++ src/sh/run_chrprefix_test.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+# Test if chr prefixes are detected correctly automatically
+#
+
+set +e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. ${DIR}/detect_vars.sh
+
+echo "Test for chr prefix detection  ${HCVERSION} from ${HCDIR}"
+
+TMP_OUT=`mktemp -t happy.XXXXXXXXXX`
+
+# run hap.py
+${PYTHON} ${HCDIR}/hap.py \
+			 	${DIR}/../data/numeric_chrs/truth.vcf \
+			 	${DIR}/../data/numeric_chrs/query.vcf \
+                -f ${DIR}/../data/numeric_chrs/fp.bed \
+			 	-o ${TMP_OUT} -P \
+			 	-X --reference ${DIR}/../data/numeric_chrs/test.fa \
+                -V \
+			 	--force-interactive
+
+if [[ $? != 0 ]]; then
+	echo "hap.py failed!"
+	exit 1
+fi
+
+diff ${TMP_OUT}.counts.csv ${DIR}/../data/numeric_chrs/expected.counts.csv
+if [[ $? != 0 ]]; then
+	echo "Counts differ! diff ${TMP_OUT}.counts.csv ${DIR}/../data/numeric_chrs/expected.counts.csv"
+	exit 1
+fi
+
+gunzip -c ${TMP_OUT}.vcf.gz | grep -v ^# > ${TMP_OUT}.vcf
+diff ${TMP_OUT}.vcf ${DIR}/../data/numeric_chrs/expected.vcf
+if [[ $? != 0 ]]; then
+	echo "Variants differ! diff ${TMP_OUT}.vcf ${DIR}/../data/numeric_chrs/expected.vcf"
+	exit 1
+else
+    echo "Numeric chr test successful"
+    rm -rf ${TMP_OUT}.*
+fi
+
+# run hap.py
+${PYTHON} ${HCDIR}/hap.py \
+			 	${DIR}/../data/numeric_chrs/chrtruth.vcf \
+			 	${DIR}/../data/numeric_chrs/chrquery.vcf \
+                -f ${DIR}/../data/numeric_chrs/chrfp.bed \
+			 	-o ${TMP_OUT} -P \
+			 	-X --reference ${DIR}/../data/numeric_chrs/chrtest.fa \
+                -V \
+			 	--force-interactive
+
+if [[ $? != 0 ]]; then
+	echo "hap.py failed!"
+	exit 1
+fi
+
+diff ${TMP_OUT}.counts.csv ${DIR}/../data/numeric_chrs/expected.counts.csv
+if [[ $? != 0 ]]; then
+	echo "Counts differ! diff ${TMP_OUT}.counts.csv ${DIR}/../data/numeric_chrs/expected.counts.csv"
+	exit 1
+fi
+
+gunzip -c ${TMP_OUT}.vcf.gz | grep -v ^# > ${TMP_OUT}.vcf
+diff ${TMP_OUT}.vcf ${DIR}/../data/numeric_chrs/chrexpected.vcf
+if [[ $? != 0 ]]; then
+	echo "Variants differ! diff ${TMP_OUT}.vcf ${DIR}/../data/numeric_chrs/chrexpected.vcf"
+	exit 1
+else
+    echo "Chr prefixed test successful"
+    rm -rf ${TMP_OUT}.*
+fi
+
+# run hap.py
+${PYTHON} ${HCDIR}/hap.py \
+			 	${DIR}/../data/numeric_chrs/chrtruth.vcf \
+			 	${DIR}/../data/numeric_chrs/query.vcf \
+                -f ${DIR}/../data/numeric_chrs/chrfp.bed \
+			 	-o ${TMP_OUT} -P \
+			 	-X --reference ${DIR}/../data/numeric_chrs/chrtest.fa \
+                -V \
+			 	--force-interactive
+
+if [[ $? != 0 ]]; then
+	echo "hap.py failed!"
+	exit 1
+fi
+
+diff ${TMP_OUT}.counts.csv ${DIR}/../data/numeric_chrs/expected.counts.csv
+if [[ $? != 0 ]]; then
+	echo "Counts differ! diff ${TMP_OUT}.counts.csv ${DIR}/../data/numeric_chrs/expected.counts.csv"
+	exit 1
+fi
+
+gunzip -c ${TMP_OUT}.vcf.gz | grep -v ^# > ${TMP_OUT}.vcf
+diff ${TMP_OUT}.vcf ${DIR}/../data/numeric_chrs/chrexpected.vcf
+if [[ $? != 0 ]]; then
+	echo "Variants differ! diff ${TMP_OUT}.vcf ${DIR}/../data/numeric_chrs/chrexpected.vcf"
+	exit 1
+else
+    echo "Chr prefixed truth test successful"
+    rm -rf ${TMP_OUT}.*
+fi
+
