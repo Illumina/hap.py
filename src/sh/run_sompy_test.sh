236abf5c7077fcff0cfb35c42019d970d58c4aef
Mar Gonzalez-Porta <mgonzalez@illumina.com>
2016-04-27 11:49:36 +0100

    HAP-187: automatically add chr prefix unless otherwise specified

--- src/sh/run_sompy_test.sh
+++ src/sh/run_sompy_test.sh
@@ -7,6 +7,8 @@ set +e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . ${DIR}/detect_vars.sh
 
+#####
+
 echo "Somatic comparison test for ${HCVERSION} from ${HCDIR}"
 
 TMP_OUT=`mktemp -t sompy.XXXXXXXXXX`
@@ -27,6 +29,8 @@ if [[ $? != 0 ]]; then
 	echo "Output counts differ diff  ${TMP_OUT}.stats.csv ${DIR}/../../example/sompy/stats.csv !"
 	exit 1
 fi
+
+#####
 
 echo "Somatic comparison test with FP regions for ${HCVERSION} from ${HCDIR}"
 
@@ -49,6 +53,8 @@ if [[ $? != 0 ]]; then
 	exit 1
 fi
 
+#####
+
 echo "Somatic comparison test with FP regions + fixchr_truth for ${HCVERSION} from ${HCDIR}"
 
 TMP_OUT=`mktemp -t sompy.XXXXXXXXXX`
@@ -70,6 +76,8 @@ if [[ $? != 0 ]]; then
 	echo "Output counts differ diff  ${TMP_OUT}.stats.csv ${DIR}/../../example/sompy/stats_fpr.csv !"
 	exit 1
 fi
+
+#####
 
 # run som.py
 echo "${PYTHON} ${HCDIR}/som.py \
@@ -98,6 +106,125 @@ if [[ $? != 0 ]]; then
 	echo "Output features differ:  diff ${TMP_OUT}.features.csv ${DIR}/../../example/sompy/features.csv"
 	exit 1
 fi
+
+#####
+
+echo "Somatic comparison test with FP regions + fixchr_truth + fixchr_query for ${HCVERSION} from ${HCDIR}"
+
+TMP_OUT=`mktemp -t sompy.XXXXXXXXXX`
+
+# run som.py
+${PYTHON} ${HCDIR}/som.py \
+                  ${DIR}/../../example/sompy/PG_admix_truth_snvs_nochr.vcf.gz \
+                  ${DIR}/../../example/sompy/strelka_admix_snvs_nochr.vcf.gz \
+                  --fixchr-truth \
+                  --fixchr-query \
+                  -o ${TMP_OUT} -P -f ${DIR}/../../example/sompy/FP_admix_nochr.bed.gz --count-unk
+
+if [[ $? != 0 ]]; then
+    echo "som.py failed!"
+    exit 1
+fi
+
+${PYTHON} ${DIR}/compare_sompy.py ${TMP_OUT}.stats.csv ${DIR}/../../example/sompy/stats_fpr.csv
+if [[ $? != 0 ]]; then
+    echo "Output counts differ diff  ${TMP_OUT}.stats.csv ${DIR}/../../example/sompy/stats_fpr.csv !"
+    exit 1
+fi
+
+#####
+
+echo "Somatic comparison test with FP regions + automatic fixchr_query (no chr prefix in query nor truthset) for ${HCVERSION} from ${HCDIR}"
+
+TMP_OUT=`mktemp -t sompy.XXXXXXXXXX`
+
+# run som.py
+${PYTHON} ${HCDIR}/som.py \
+                  ${DIR}/../../example/sompy/PG_admix_truth_snvs_nochr.vcf.gz \
+                  ${DIR}/../../example/sompy/strelka_admix_snvs_nochr.vcf.gz \
+                  -o ${TMP_OUT} -P -f ${DIR}/../../example/sompy/FP_admix_nochr.bed.gz --count-unk
+
+if [[ $? != 0 ]]; then
+    echo "som.py failed!"
+    exit 1
+fi
+
+${PYTHON} ${DIR}/compare_sompy.py ${TMP_OUT}.stats.csv ${DIR}/../../example/sompy/stats_fpr.csv
+if [[ $? != 0 ]]; then
+    echo "Output counts differ diff  ${TMP_OUT}.stats.csv ${DIR}/../../example/sompy/stats_fpr.csv !"
+    exit 1
+fi
+
+#####
+
+echo "Somatic comparison test with FP regions + automatic fixchr_query (chr prefix in query and truthset) for ${HCVERSION} from ${HCDIR}"
+
+TMP_OUT=`mktemp -t sompy.XXXXXXXXXX`
+
+# run som.py
+${PYTHON} ${HCDIR}/som.py \
+                  ${DIR}/../../example/sompy/PG_admix_truth_snvs.vcf.gz \
+                  ${DIR}/../../example/sompy/strelka_admix_snvs.vcf.gz \
+                  -o ${TMP_OUT} -P -f ${DIR}/../../example/sompy/FP_admix_nochr.bed.gz --count-unk
+
+if [[ $? != 0 ]]; then
+    echo "som.py failed!"
+    exit 1
+fi
+
+${PYTHON} ${DIR}/compare_sompy.py ${TMP_OUT}.stats.csv ${DIR}/../../example/sompy/stats_fpr.csv
+if [[ $? != 0 ]]; then
+    echo "Output counts differ diff  ${TMP_OUT}.stats.csv ${DIR}/../../example/sompy/stats_fpr.csv !"
+    exit 1
+fi
+
+#####
+
+echo "Somatic comparison test with FP regions + automatic fixchr_query (chr prefix in query only) for ${HCVERSION} from ${HCDIR}"
+
+TMP_OUT=`mktemp -t sompy.XXXXXXXXXX`
+
+# run som.py
+${PYTHON} ${HCDIR}/som.py \
+                  ${DIR}/../../example/sompy/PG_admix_truth_snvs_nochr.vcf.gz \
+                  ${DIR}/../../example/sompy/strelka_admix_snvs.vcf.gz \
+                  -o ${TMP_OUT} -P -f ${DIR}/../../example/sompy/FP_admix_nochr.bed.gz --count-unk
+
+if [[ $? != 0 ]]; then
+    echo "som.py failed!"
+    exit 1
+fi
+
+${PYTHON} ${DIR}/compare_sompy.py ${TMP_OUT}.stats.csv ${DIR}/../../example/sompy/stats_fpr.csv
+if [[ $? != 0 ]]; then
+    echo "Output counts differ diff  ${TMP_OUT}.stats.csv ${DIR}/../../example/sompy/stats_fpr.csv !"
+    exit 1
+fi
+
+#####
+
+echo "Somatic comparison test with FP regions + automatic fixchr_query (chr prefix in truthset only) for ${HCVERSION} from ${HCDIR}"
+
+TMP_OUT=`mktemp -t sompy.XXXXXXXXXX`
+
+# run som.py
+${PYTHON} ${HCDIR}/som.py \
+                  ${DIR}/../../example/sompy/PG_admix_truth_snvs.vcf.gz \
+                  ${DIR}/../../example/sompy/strelka_admix_snvs_nochr.vcf.gz \
+                  -o ${TMP_OUT} -P -f ${DIR}/../../example/sompy/FP_admix_nochr.bed.gz --count-unk
+
+if [[ $? != 0 ]]; then
+    echo "som.py failed!"
+    exit 1
+fi
+
+${PYTHON} ${DIR}/compare_sompy.py ${TMP_OUT}.stats.csv ${DIR}/../../example/sompy/stats_fpr.csv
+if [[ $? != 0 ]]; then
+    echo "Output counts differ diff  ${TMP_OUT}.stats.csv ${DIR}/../../example/sompy/stats_fpr.csv !"
+    exit 1
+fi
+
+#####
 
 rm -f ${TMP_OUT}*
 
