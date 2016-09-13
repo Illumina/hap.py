7019fb222d1602bfb00115b6c8432bb1b91af078
Stathis <ekanterakis@illumina.com>
2016-09-13 10:22:16 +0100

    HAP-238 using same technique as in vcfextract.py

--- src/python/Tools/vcfcallerinfo.py
+++ src/python/Tools/vcfcallerinfo.py
@@ -45,8 +45,9 @@ class CallerInfo(object):
         tf.close()
         vfh = {}
         try:
-            sp = subprocess.Popen("vcfhdr2json %s %s" % (vcfname, tf.name),
-                                  shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            sp = subprocess.Popen("vcfhdr2json %s %s" % (vcfname.replace(" ", "\\ "), 
+                tf.name.replace(" ", "\\ ")),
+                shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             o, e = sp.communicate()
 
             if sp.returncode != 0:
@@ -120,7 +121,7 @@ class CallerInfo(object):
         """ Extract aligner information from a BAM file
         :param bamfile: name of BAM file
         """
-        sp = subprocess.Popen("samtools view -H %s" % bamfile,
+        sp = subprocess.Popen("samtools view -H %s" % bamfile.replace(" ", "\\ "),
                               shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         o, e = sp.communicate()
 
