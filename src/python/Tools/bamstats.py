d91aa74fe9cdebce68335d9885e40bc6f08accc2
Peter Krusche <pkrusche@illumina.com>
2016-09-23 13:55:28 +0100

    HAP-242 improved bamstats

--- src/python/Tools/bamstats.py
+++ src/python/Tools/bamstats.py
@@ -3,7 +3,7 @@
 #
 # Copyright (c) 2010-2015 Illumina, Inc.
 # All rights reserved.
-# 
+#
 # This file is distributed under the simplified BSD license.
 # The full text can be found here (and in LICENSE.txt in the root folder of
 # this distribution):
@@ -12,6 +12,7 @@
 
 import pysam
 import pandas
+import numpy as np
 
 
 def bamStats(bamfile):
@@ -19,8 +20,12 @@ def bamStats(bamfile):
     istats = pysam.idxstats(bamfile)
     result = []
     samfile = pysam.Samfile(bamfile, "rb")
+
     for x in istats:
         xs = x.replace("\n", "").split("\t")
+        if len(xs) < 4:
+            logging.warn("Ignoring invalid stats line: %s" % x)
+            continue
         rec = {
             "CHROM": xs[0],
             "NT": int(xs[1]),
@@ -29,8 +34,9 @@ def bamStats(bamfile):
             "READLEN": 0,
             "COVERAGE": 0.0,
         }
+
         count = 0
-        rls = 0.0        
+        rls = 0.0
         try:
             for read in samfile.fetch(xs[0]):
                 rls += float(read.rlen)
@@ -46,6 +52,33 @@ def bamStats(bamfile):
         result.append(rec)
 
     if result:
-        return pandas.DataFrame(result, columns=["CHROM", "NT", "MAPPED", "UNMAPPED", "READLEN", "COVERAGE"])
+        result = pandas.DataFrame(result, columns=["CHROM", "NT", "MAPPED", "UNMAPPED", "READLEN", "COVERAGE"])
+        min_readlen = np.min(result[result["READLEN"] > 0]["READLEN"])
+        max_readlen = np.max(result[result["READLEN"] > 0]["READLEN"])
+        if min_readlen != max_readlen:
+            logging.warn("Read lengths differ within the same BAM file: %s" % str(result["READLEN"].unique()))
+
+        agg_result = [{
+            "CHROM": "TOTAL",
+            "NT": np.sum(result["NT"]),
+            "MAPPED": np.sum(result["MAPPED"]),
+            "UNMAPPED": np.sum(result["UNMAPPED"]),
+            "READLEN": (max_readlen + min_readlen) / 2.0,
+        }]
+        agg_result[-1]["COVERAGE"] = np.sum(result["MAPPED"].multiply(result["READLEN"])) / np.sum(result["NT"])
+
+        auto_result = result[result["CHROM"].str.match(r"^(?:chr)?[0-9]+$")]
+        min_readlen = np.min(auto_result[auto_result["READLEN"] > 0]["READLEN"])
+        max_readlen = np.max(auto_result[auto_result["READLEN"] > 0]["READLEN"])
+        agg_result.append({
+            "CHROM": "AUTOSOME",
+            "NT": np.sum(auto_result["NT"]),
+            "MAPPED": np.sum(auto_result["MAPPED"]),
+            "UNMAPPED": np.sum(auto_result["UNMAPPED"]),
+            "READLEN": (max_readlen + min_readlen) / 2.0,
+        })
+        agg_result[-1]["COVERAGE"] = np.sum(auto_result["MAPPED"].multiply(auto_result["READLEN"])) / np.sum(auto_result["NT"])
+
+        return pandas.concat([result, pandas.DataFrame(agg_result, columns=["CHROM", "NT", "MAPPED", "UNMAPPED", "READLEN", "COVERAGE"])]).set_index(["CHROM"])
     else:
         return pandas.DataFrame(columns=["CHROM", "NT", "MAPPED", "UNMAPPED", "READLEN", "COVERAGE"])
