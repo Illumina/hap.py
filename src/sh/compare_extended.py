9fb5d5e251fb37423402f04f3488a59a763bdd84
Peter Krusche <pkrusche@illumina.com>
2017-03-01 11:41:51 +0000

    HAP-280 add test case that checks extended output

--- src/sh/compare_extended.py
+++ src/sh/compare_extended.py
@@ -3,37 +3,38 @@
 # Compare two extended csv files
 
 import sys
+import csv
 
 def csvread(filename):
     f = open(filename)
-
-    header = None
-    rows = []
-    for l in f:
-        l = l.replace("\n", "")
-        if not header:
-            header = l.split(",")
-        else:
-            rows.append(l.split(","))
-
+    freader = csv.DictReader(f)
     data = {}
-    labels = set()
-    for r in rows:
-        label = r[0]
+    label_columns = ["Type", 
+                     "Subtype", 
+                     "Subset",
+                     "Filter", 
+                     "Genotype",
+                     "QQ.Field",
+                     "QQ"]
+
+    for l in freader:
+        record = dict(l)
+        label = "_".join([record[k] for k in label_columns])
         if "hap.py" in label:
             # ignore version line
             continue
-        if label in labels:
-            continue
-        labels.add(label)
-        for i in xrange(1, len(r)):
-            if header[i] not in data:
-                data[header[i]] = {}
+
+        for k in record.keys():
             try:
-                data[header[i]][label] = float(r[i])
+                record[k] = int(record[k])
             except:
-                data[header[i]][label] = r[i]
-    f.close()
+                try:
+                    record[k] = float(record[k])
+                except:
+                    if record[k] == "." or record[k] == "":
+                        record[k] = float("NaN")
+        data[label] = record
+
     return data
 
 
@@ -41,56 +42,80 @@ def main():
     data1 = csvread(sys.argv[1])
     data2 = csvread(sys.argv[2])
 
+    all_keys_1 = set()
+    for l in data1.values():
+        all_keys_1 |= set(l.keys())
+    all_keys_2 = set()
+    for l in data2.values():
+        all_keys_2 |= set(l.keys())
+
+    if len(all_keys_1 - all_keys_2) or len(all_keys_2 - all_keys_1):
+        print >> sys.stderr, "ERROR -- missing metrics:"
+        print >> sys.stderr, "-------------------------\n"
+        print >> sys.stderr, "In 1 but not 2: %s" % str(all_keys_1 - all_keys_2)
+        print >> sys.stderr, "In 2 but not 1: %s" % str(all_keys_1 - all_keys_2)
+        print >> sys.stderr, "-------------------------"
+        sys.exit(1)
+
+    all_labels_1 = set(data1.keys())
+    all_labels_2 = set(data2.keys())
+
+    if len(all_labels_1 - all_labels_2) or len(all_labels_2 - all_labels_1):
+        print >> sys.stderr, "ERROR -- missing rows:"
+        print >> sys.stderr, "-------------------------\n"
+        print >> sys.stderr, "In 1 but not 2: %s" % str(all_labels_1 - all_labels_2)
+        print >> sys.stderr, "In 2 but not 1: %s" % str(all_labels_1 - all_labels_2)
+        print >> sys.stderr, "-------------------------"
+        sys.exit(1)
+
     different_metrics = []
 
-    for metric in ["TRUTH.TOTAL", "QUERY.TOTAL",
-                   "TRUTH.TP", "TRUTH.FN",
-                   "QUERY.FP", "QUERY.UNK",
-                   "TRUTH.TOTAL.TiTv_ratio", "QUERY.TOTAL.TiTv_ratio",
-                   "TRUTH.TOTAL.het_hom_ratio", "QUERY.TOTAL.het_hom_ratio",
-                   "METRIC.Recall", "METRIC.Precision", "METRIC.Frac_NA",
-                   "Subset.Size", "Subset.IS_CONF.Size"]:
-        if metric not in data1 and metric not in data2:
-            continue
-        for field in ["Locations.SNP",
-                      "Locations.INDEL"
-                      ]:
-            field1 = field
-            if field1 not in data1[metric]:
-                field1 = field1.replace("Locations.", "")
-            field2 = field
-            if field2 not in data2[metric]:
-                field2 = field2.replace("Locations.", "")
-            if field1 not in data1[metric] and field2 not in data2[metric]:
-                print >>sys.stderr, "Skipping %s -- not present on both sides" % field
-                continue
-            print metric + " / " + field
-            print data1[metric][field1]
-            print data2[metric][field2]
-            if metric.endswith("_ratio") and data1[metric][field1] in ["", "."] and data2[metric][field2] in ["", "."]:
-                # allow empty ratio match
-                continue
-            try:
-                data1[metric][field1] = float(data1[metric][field1])
-            except:
-                data1[metric][field1] = float("NaN")
-            try:
-                data2[metric][field2] = float(data2[metric][field2])
-            except:
-                data2[metric][field2] = float("NaN")
+    for label in all_labels_1:
+        for metric in all_keys_1:
+            print label + "/" + metric
 
-            if ("%.3g" % data1[metric][field1]) != ("%.3g" % data2[metric][field2]):
-                different_metrics.append((field,
+            v1 = data1[label][metric]
+            v2 = data2[label][metric]
+
+            if metric.endswith("_ratio"):
+                if v1 == ".":
+                    v1 = ""
+                if v2 == ".":
+                    v2 = ""
+
+            if type(v1) != type(v2):
+                different_metrics.append((label,
                                           metric,
-                                          ("%.3g" % data1[metric][field1]),
-                                          ("%.3g" % data2[metric][field2]),
-                                          data2[metric][field2] - data1[metric][field1]))
+                                          ("%s" % type(v1)),
+                                          ("%s" % type(v2)),
+                                          "type mismatch"))
+            elif type(v1) is float:
+                if ("%.3g" % v1) != ("%.3g" % v2):
+                    different_metrics.append((label,
+                                              metric,
+                                              ("%.3g" % v1),
+                                              ("%.3g" % v2),
+                                              str(v1 - v2)))
+            elif type(v1) is int:
+                if v1 != v2:
+                    different_metrics.append((label,
+                                              metric,
+                                              str(v1),
+                                              str(v2),
+                                              str(v1 - v2)))
+            else:
+                if v1 != v2:
+                    different_metrics.append((label,
+                                              metric,
+                                              str(v1),
+                                              str(v2),
+                                              "Non-numeric mismatch"))
 
     if different_metrics:
         print >> sys.stderr, "ERROR -- Metric differences detected:"
         print >> sys.stderr, "-------------------------------------\n"
         for m in different_metrics:
-            print >> sys.stderr, "%s / %s: %s != %s difference: %f" % m
+            print >> sys.stderr, "%s / %s: %s != %s difference: %s" % m
         print >> sys.stderr, "-------------------------------------"
         sys.exit(1)
 
