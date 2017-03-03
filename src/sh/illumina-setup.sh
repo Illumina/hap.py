017fa4467ccd5f20e03a8197dd3da6ed9188863b
Peter Krusche <pkrusche@illumina.com>
2017-03-03 17:39:53 +0000

    HAP-291 fix on Centos 6.5

--- src/sh/illumina-setup.sh
+++ src/sh/illumina-setup.sh
@@ -12,18 +12,16 @@ if [[ -d /illumina ]]; then
         . /etc/profile.d/modules.sh
     fi
 
-    module purge --force &> /dev/null
-
     is_lua_modules=$(module --version 2>&1 | grep Lua)
     if [[ -z $is_lua_modules ]]; then
         unset MODULEPATH
-        module use /illumina/sync/software/unofficial/HPCBIOS/2016q3/modules/all &> /dev/null
+        module use /illumina/sync/software/thirdparty/HPCBIOS/modules/all &> /dev/null
+        module use /illumina/sync/software/unofficial/HPCBIOS/2016q2/modules/all &> /dev/null
     else
         module purge  &> /dev/null
-        module load --force HPCBIOS/2016q3 &> /dev/null
+        module load --force HPCBIOS/2016q2 &> /dev/null
         module load sge/2011.11p1
     fi
-    module load Java/1.8.0_40
 fi
 
 module load CMake/3.2.1-GCC-4.9.2
