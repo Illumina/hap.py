9fa6f7beef989e2ac2993a1b40466e408de1c6ce
Mar Gonzalez-Porta <mgonzalez@illumina.com>
2017-12-18 11:26:23 +0000

    revert to original; update to be done as part of HAP-346

--- src/sh/illumina-setup.sh
+++ src/sh/illumina-setup.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 #
-# Load modules to support compiling
-#
 # Author: Peter Krusche <pkrusche@illumina.com>
 
-if [[ -z "$(module --version 2>&1 | grep Lua)" ]]; then
-    . /etc/profile.d/modules.sh
-    module purge
-    unset MODULEPATH
-    module use /illumina/sync/software/thirdparty/HPCBIOS/modules/all
-    module use /illumina/development/grmpy/easybuild_centos65/modules/all
-    module load CMake/3.2.1-GCC-4.9.2 &> /dev/null
-    module load XZ
-    module unload GCC
-    module load GCC/5.2.0
-    module load libtool &> /dev/null
-    module load Automake &> /dev/null
-    module load  /illumina/sync/software/thirdparty/HPCBIOS.20150401/modules/all/Valgrind/3.8.1-goolf-1.4.10 &> /dev/null
-else
-    module --force purge
-    module unload use.own
-    module unload use.own.eb
-    module load HPCBIOS/2017q2-el7 &> /dev/null
-    module load Valgrind
-    module load CMake/3.7.2-GCCcore-6.3.0
-    module load Python/3.6.1-intel-2017a
-    module load libtool
-    module load Automake
-    module load Java/1.8.0_40
+# setup cmake environment vars for development on ilmn cluster
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [[ -d /illumina ]]; then
+    if [[ "$(grep '6\.5' /etc/centos-release)" ]]; then
+        . /etc/profile.d/modules.sh
+    fi
+
+    is_lua_modules=$(module --version 2>&1 | grep Lua)
+    if [[ -z $is_lua_modules ]]; then
+        unset MODULEPATH
+        module use /illumina/sync/software/thirdparty/HPCBIOS/modules/all
+        module load CMake/3.2.1-GCC-4.9.2
+        module load Java/1.8.0_40
+    else
+        ml purge  &> /dev/null
+        ml HPCBIOS/2015q2
+        ml sge/2011.11p1
+        ml CMake/3.2.1-GCC-4.9.2
+        ml Java/1.8.0_40
+    fi
 fi
+
+export ANT_HOME=/illumina/thirdparty/ant/apache-ant-1.9.2
+export EXTRA_CMAKE_OPTS="-DUSE_SGE=ON -DBUILD_VCFEVAL=ON -DVCFEVAL_WRAPPER=${DIR}/rtg-wrapper.sh"
+export HAPPY_PATCH_SAMTOOLS="yes"
